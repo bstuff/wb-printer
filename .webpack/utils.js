eab9aa0e1d50ad1eaaf39a6aa0eaad358cbd7104
Andrew Lavrentev <andrew.lavrentev@gmail.com>
2021-03-21 22:18:09 +0300

    public path (#4)

--- .webpack/utils.js
+++ .webpack/utils.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const getPublicPath = () => {
-  return process.env.PUBLIC_PATH || '/';
+  return process.env.PUBLIC_PATH || '';
 };
 
 const getOutputPath = (projectDir) => {
